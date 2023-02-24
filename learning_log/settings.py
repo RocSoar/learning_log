bf07fea882d9dae465251cb1c6d2ffb60330b722
RocSoar <lsyyd3454@outlook.com>
2023-02-24 14:03:19 +0800

    Delete heroku settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -133,9 +133,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "users:login"
 
 # Heroku 设置
-import django_heroku
+#import django_heroku
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 if os.environ.get("DEBUG") == "TRUE":
     DEBUG = True
